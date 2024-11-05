4855113df1a2b41410a17dc6d25b582f600b8163
AshleyMiller79 <ipekbilir79@gmail.com>
2024-11-05 13:14:27 +0300

    aa

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 
 import './App.css';
+import Msg from './msg/Msg';
+
+
+
 
 function App() {
   return (
     <div >
-  Apppp
+  Apppp merhaba
+
+ <Msg/>
     </div>
   );
 }
---
+++ src/msg/Msg.jsx
@@ -0,0 +1,13 @@
+
+import "./Msg.css"
+
+const Msg = () => {
+    
+  return (
+    <div>
+        merhaba msg
+    </div>
+  )
+}
+
+export default Msg
